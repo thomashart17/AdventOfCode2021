8aba00ac6031662c7d4910e8913aad8ba62e680a
Thomas Hart <thomas.hart@uwaterloo.ca>
2021-12-02 03:24:09 -0500

    Added day 2 part 2

---
+++ Day 2/day2part2.py
@@ -0,0 +1,22 @@
+# Advent of Code 2021 Day 2 Part 2
+# Author: Thomas Hart
+
+def calc_position(file):
+    commands = []
+    with open(file) as f:
+        for line in f:
+            commands.append(line.split())
+    x, y = 0, 0
+    aim = 0
+    for command in commands:
+        if command[0] == "forward":
+            x += int(command[1])
+            y += aim*int(command[1])
+        elif command[0] == "down":
+            aim += int(command[1])
+        elif command[0] == "up":
+            aim -= int(command[1])
+    return x, y
+
+x, y = calc_position("input.txt")
+print(x*y)
